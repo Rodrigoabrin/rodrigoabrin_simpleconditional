3335c14dac2d7c11602fb41580ec5d96336cbf13
Rodrigoabrin <70342493+Rodrigoabrin@users.noreply.github.com>
2020-09-24 13:43:06 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+let celsius = 0
+input.onButtonPressed(Button.A, function () {
+    if (celsius == 10) {
+        basic.showNumber(celsius)
+        basic.showString("Ski time!")
+    } else if (celsius == 20) {
+        basic.showNumber(celsius)
+        basic.showString("Picnic time!")
+    } else if (celsius == 30) {
+        basic.showNumber(celsius)
+        basic.showString("Beach time!")
+    } else if (celsius == 40) {
+        basic.showNumber(celsius)
+        basic.showString("It is better to stay at home")
+    } else {
+        basic.showNumber(celsius)
+    }
+})
+basic.forever(function () {
+    celsius = input.temperature()
+})
